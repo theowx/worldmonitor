927541507fd59ed7705c9ca6b1d29900771ade73
theowx <theyothegamer@gmail.com>
2026-02-13 18:51:32 -0500

    Fix @ alias for ESM + GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  base: '/worldmonitor/'
+  base: '/worldmonitor/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src')
+    }
+  }
 })
