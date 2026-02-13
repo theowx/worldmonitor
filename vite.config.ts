f3f7219280d12d83c49b9f615f6b9ba97c839440
theowx <theyothegamer@gmail.com>
2026-02-13 18:31:28 -0500

    Fix import paths + GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-  base: '/worldmonitor/'
+  base: '/worldmonitor/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 })
