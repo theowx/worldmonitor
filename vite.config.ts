9dd45ad542a3a2ba1b8a06f136d67d75d403b989
theowx <theyothegamer@gmail.com>
2026-02-13 18:37:45 -0500

    Add React plugin + base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/worldmonitor/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+  plugins: [react()],
+  base: '/worldmonitor/'
 })
