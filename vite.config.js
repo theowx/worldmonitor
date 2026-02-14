886352a861fba1cf3ed2e4634112845724bafb60
theowx <theyothegamer@gmail.com>
2026-02-13 19:01:45 -0500

    Fix Rollup alias array format

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/worldmonitor/'
+  base: '/worldmonitor/',
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' }
+    ]
+  }
 })
